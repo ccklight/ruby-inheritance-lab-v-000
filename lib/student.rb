13cf29674d684c13c36a29a825407e909bee3132
Cheryl K <cherylkelson@icloud.com>
2018-10-08 14:13:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,19 +9,14 @@ class Student < User
   end 
 
   def knowledge
-    knowledge
+    @knowledge
   end 
   
 end 
 
 ****
-# class Student < User
 
-#   attr_accessor :knowledge
 
-#   def initialize
-#     @knowledge = []
-#   end
 
 #   def learn(knowledge)
 #     @knowledge << knowledge
